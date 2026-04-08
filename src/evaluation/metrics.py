af1c244a7f8bdb9fd735bcf9a6e4086626bb5f69
belchetz <49354737+belchetz@users.noreply.github.com>
2026-04-08 13:28:54 -0700

    chore: add prototypes for metric functions

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,2 +1,7 @@
-"""Evaluation metrics: Adversarial Success Rate (Fooling Rate, etc)"""
+"""Evaluation metrics: Adversarial Success Rate (Fooling Rate), FID."""
 
+def calculate_fooling_rate(generator, target_model, num_samples):
+    pass
+
+def calculate_fid(real_data, fake_data):
+    pass
